4866b9422cda391eb1b42eb7db7487481a3a0474
Rick Warren <rickbw@users.noreply.github.com>
2013-06-06 23:32:01 -0700

    Added sync and future resource deletion capabilities

--- src/main/java/rickbw/crud/http/SyncJerseyMapDeleter.java
+++ src/main/java/rickbw/crud/http/SyncJerseyMapDeleter.java
@@ -2,7 +2,7 @@ package rickbw.crud.http;
 
 import java.net.URI;
 
-import rickbw.crud.sync.SyncMapResourceProvider;
+import rickbw.crud.sync.SyncMapResourceDeleter;
 import com.google.common.base.Preconditions;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientHandlerException;
@@ -11,22 +11,22 @@ import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
 
 
-public final class SyncJerseyMapGetter
+public final class SyncJerseyMapDeleter
 extends JerseyResponseCloser
-implements SyncMapResourceProvider<URI, ClientResponse> {
+implements SyncMapResourceDeleter<URI, ClientResponse> {
 
     private final Client restClient;
 
 
-    public SyncJerseyMapGetter(final Client restClient) {
+    public SyncJerseyMapDeleter(final Client restClient) {
         this.restClient = Preconditions.checkNotNull(restClient);
     }
 
     @Override
-    public ClientResponse get(final URI uri) throws UniformInterfaceIOException, ClientHandlerIOException {
+    public ClientResponse delete(final URI uri) throws UniformInterfaceIOException, ClientHandlerIOException {
         final WebResource webResource = this.restClient.resource(uri);
         try {
-            final ClientResponse response = webResource.get(ClientResponse.class);
+            final ClientResponse response = webResource.delete(ClientResponse.class);
             return response;
         } catch (final UniformInterfaceException uix) {
             throw new UniformInterfaceIOException(uix);
