1d62053328aeb0497d7c4ab1ff539606be3ead74
Rick Warren <rickbw@users.noreply.github.com>
2013-06-07 07:07:01 -0700

    Added full and partial update capabilities

--- src/main/java/rickbw/crud/http/SyncJerseyMapClient.java
+++ src/main/java/rickbw/crud/http/SyncJerseyMapClient.java
@@ -4,6 +4,8 @@ import java.net.URI;
 
 import rickbw.crud.sync.SyncMapResourceDeleter;
 import rickbw.crud.sync.SyncMapResourceProvider;
+import rickbw.crud.sync.SyncMapResourceSetter;
+import rickbw.crud.sync.SyncMapResourceUpdater;
 import com.google.common.base.Preconditions;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientHandlerException;
@@ -14,7 +16,9 @@ import com.sun.jersey.api.client.WebResource;
 
 public final class SyncJerseyMapClient
 implements SyncMapResourceProvider<URI, ClientResponse>,
-           SyncMapResourceDeleter<URI, ClientResponse> {
+           SyncMapResourceDeleter<URI, ClientResponse>,
+           SyncMapResourceSetter<URI, Object, ClientResponse>,
+           SyncMapResourceUpdater<URI, Object, ClientResponse> {
 
     private final Client restClient;
 
@@ -41,6 +45,32 @@ implements SyncMapResourceProvider<URI, ClientResponse>,
         final WebResource webResource = this.restClient.resource(uri);
         try {
             final ClientResponse response = webResource.delete(ClientResponse.class);
+            return response;
+        } catch (final UniformInterfaceException uix) {
+            throw new UniformInterfaceIOException(uix);
+        } catch (final ClientHandlerException chx) {
+            throw new ClientHandlerIOException(chx);
+        }
+    }
+
+    @Override
+    public ClientResponse put(final URI uri, final Object update) throws UniformInterfaceIOException, ClientHandlerIOException {
+        final WebResource webResource = this.restClient.resource(uri);
+        try {
+            final ClientResponse response = webResource.put(ClientResponse.class, update);
+            return response;
+        } catch (final UniformInterfaceException uix) {
+            throw new UniformInterfaceIOException(uix);
+        } catch (final ClientHandlerException chx) {
+            throw new ClientHandlerIOException(chx);
+        }
+    }
+
+    @Override
+    public ClientResponse update(final URI uri, final Object update) throws UniformInterfaceIOException, ClientHandlerIOException {
+        final WebResource webResource = this.restClient.resource(uri);
+        try {
+            final ClientResponse response = webResource.post(ClientResponse.class, update);
             return response;
         } catch (final UniformInterfaceException uix) {
             throw new UniformInterfaceIOException(uix);
