235166dcde523e4af877a7bf7c45cb99a61e342b
Rick Warren <rickbw@users.noreply.github.com>
2014-03-01 17:38:46 -0800

    Added license to all files.

--- src/main/java/rickbw/crud/http/JerseyReadableResource.java
+++ src/main/java/rickbw/crud/http/JerseyReadableResource.java
@@ -1,13 +1,28 @@
+/* Copyright 2013–2014 Rick Warren
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package rickbw.crud.http;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 
-import rickbw.crud.ReadableResource;
-import rickbw.crud.util.AsyncObservationFunction;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.UniformInterface;
 
+import rickbw.crud.ReadableResource;
+import rickbw.crud.util.AsyncObservationFunction;
 import rx.Observable;
 import rx.Observer;
 import rx.Subscription;
