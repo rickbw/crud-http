bbf64adfbc85f1c968dc0204480dea3a34bc63b7
Rick Warren <rickbw@users.noreply.github.com>
2014-05-02 06:27:48 -0700

    Fixed obsolete documentation

--- src/main/java/rickbw/crud/http/package-info.java
+++ src/main/java/rickbw/crud/http/package-info.java
@@ -1,11 +1,7 @@
 /**
  * This package contains implementations of the interfaces in the package
  * {@link rickbw.crud} in terms of the Jersey HTTP
- * library. It contains both
- * {@link rickbw.crud.Resource}s (e.g.
- * {@link rickbw.crud.http.JerseyReadableResource})
- * and {@link rickbw.crud.ResourceProvider}s (e.g.
- * {@link rickbw.crud.http.JerseyReadableResourceProvider}).
+ * library.
  * Resources are asynchronous, and use the
  * {@link java.util.concurrent.ExecutorService} from the
  * {@link com.sun.jersey.api.client.Client} itself.
